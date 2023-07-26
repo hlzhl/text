c82e225b802ec29f839b8b1b524d0be1123129d0
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2023-07-26 10:20:48 +0800

    Update onedrive_mount.sh

--- onedrive_mount.sh
+++ onedrive_mount.sh
@@ -1,13 +1,19 @@
 
 export RCLONE_CONFIG_PASS=$1
-wget -q https://raw.githubusercontent.com/zhlhlf/text/main/rclone.conf > /dev/null
+wget -q https://raw.github.com/zhlhlf/text/main/rclone.conf > /dev/null
 log=$(curl -s https://rclone.org/install.sh | sudo bash)
 mkdir /home/runner/.config/rclone/ || echo "明"
 mv rclone.conf /home/runner/.config/rclone/rclone.conf
 mkdir zhlhlf || echo "明"
 rclone mount onedrive:/$2 ./zhlhlf --umask 000 --daemon
-echo "\n"
-ls 666
-echo "\n"
+echo -e "\n"
+
+for i in $(ls 666)
+do
+sizel=$(du -sb "666/$i" | awk '{print $1}')
+echo "$(( $sizel / 1024/1024)) m   $i" 
+done
+
+echo -e "\n"
 rclone copy 666 zhlhlf/ 
 ls zhlhlf
