7f1d0eda27d60d22c1d4cbf69ff6a57a674abe21
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2023-09-08 18:12:13 +0800

    Create ph.sh

---
+++ ph.sh
@@ -0,0 +1,32 @@
+rm -rf ph.py
+
+cat >ph.py <<end 
+import youtube_dl
+import sys
+
+def download(link):
+    dir = 'phv/%(title)s.%(ext)s'
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': dir
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+
+if __name__ == "__main__":
+    for i in sys.argv[1:]:
+        download(i)
+end
+
+if [ "a$1" = "a" ];then
+    echo "
+    sh ph.sh http://xxx http://xxx "
+    exit
+fi
+
+pip install youtube_dl
+    
+for i in $@
+do
+python ph.py $i
+done
