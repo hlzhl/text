5225e251027af3581732bfb3d867c3720bfdb614
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2023-09-12 14:57:22 +0800

    Update ph.sh

--- ph.sh
+++ ph.sh
@@ -1,6 +1,6 @@
-if [ "a$1" = "a" ];then
-    echo "
-    sh ph.sh http://xxx http://xxx "
+
+if [ ! "$1" ];then
+    echo "       sh ph.sh http://xxx http://xxx "
     exit
 fi
 
@@ -17,3 +17,22 @@ do
     youtube-dl -o '%(title)s.%(ext)s' -f best $i 
 done
 cd ../
+
+
+list=$(find 666/*)
+i=1
+while true
+do
+tt=$(sed -n ${i}p <<end
+$list
+end
+)
+i=$(($i+1))
+if [ ! $tt ];then
+    break
+fi
+
+sizel=$(du -sb "$tt" | awk '{print $1}')
+echo "$(( $sizel / 1024/1024)) m   $(basename "$tt")"
+done
+
