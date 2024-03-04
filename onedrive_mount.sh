fabecbcdf62181b7fd59a75345c6c372ed59076f
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-03-04 12:58:06 +0800

    Update onedrive_mount.sh

--- onedrive_mount.sh
+++ onedrive_mount.sh
@@ -2,7 +2,7 @@
 export RCLONE_CONFIG_PASS=$1
 rm -rf rclone.conf
 wget -q https://raw.github.com/zhlhlf/text/main/rclone.conf > /dev/null
-curl -s https://rclone.org/install.sh | sudo bash >>> /dev/null
+curl -s https://rclone.org/install.sh | sudo bash > /dev/null
 rm -rf /home/runner/.config/rclone/
 mkdir /home/runner/.config/rclone/
 mv rclone.conf /home/runner/.config/rclone/rclone.conf
