9da9d6ce20d01fcb70e698dd77ad3738ed754463
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-03-04 10:41:52 +0800

    Add files via upload
    系统阉割工程

---
+++ sys-del_project/del_dataEncryption_only.sh
@@ -0,0 +1,13 @@
+del_key(){
+   for i in "vendor/etc/fstab.qcom" "boot/ramdisk/fstab.qcom" "boot/ramdisk/oplus.fstab" 
+   do
+    sed -i s#avb.*system,#""#g "$i"
+    sed -i s#avb.*vendor,#""#g "$i"
+    sed -i s#,fileencryption.*metadata_encryption#""#g "$i"
+    sed -i s#,avb_keys.*pubkey#""#g "$i"
+   done
+}
+
+
+del_key #去除data加密 avb验证等
+echo "ro.setupwizard.mode=DISABLED" >> system/system/build.prop
