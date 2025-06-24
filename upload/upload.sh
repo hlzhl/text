9ba1ee693e0356a5ea6a4ed68ceb0df59ebcfea9
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2025-06-25 03:52:12 +0800

    Update upload.sh

--- upload/upload.sh
+++ upload/upload.sh
@@ -81,7 +81,7 @@ du -h $in_dir/*
 echo
 echo "start sync"
 echo
-rclone copy $in_dir $current_dir -P --transfers=$count --config ./alist/rclone.conf > a.log && kill -8 $pid
+rclone copy $in_dir $current_dir -P --transfers=$count --config ./alist/rclone.conf > a.log && kill -8 $pid || cat alist/a.log
 echo
 echo "mount_dir: $mount_dir"
 echo
